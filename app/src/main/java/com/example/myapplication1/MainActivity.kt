3168e889fc638d136fd78891c7e3ae21aae75d2c
glebirus4 <g.clewets@gmail.com>
2020-09-03 20:51:02 +0300

    One more new feature

--- app/src/main/java/com/example/myapplication1/MainActivity.kt
+++ app/src/main/java/com/example/myapplication1/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.example.myapplication1
 
+import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.view.View
-import android.widget.EditText
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main3.*
+import android.widget.Button
+import androidx.core.content.ContextCompat.startActivity
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    lateinit var button : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        button = findViewById(R.id.tv1)
+//        button.setOnClickListener(this)
     }
 
     fun ponedilok(view: View) {
@@ -57,7 +60,28 @@ class MainActivity : AppCompatActivity() {
     companion object {
         const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
     }
+
+
+    override fun onClick(view: View?) {
+        when(view?.id){
+            R.id.tv1->{
+                     val webpage: Uri = Uri.parse("https://github.com/glebirus4/rozklad")
+                    val intent = Intent(Intent.ACTION_VIEW, webpage)
+                    if (intent.resolveActivity(packageManager) != null) {
+                        startActivity(intent)
+                    }}
+} }
+
+    fun knigi(view: View) {
+        val intent = Intent(this, MainActivity7::class.java).apply {
+        }
+        startActivity(intent)
+    }
+
+
+
 }
+
 
 
 
---
+++ app/src/main/java/com/example/myapplication1/MainActivity7.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication1
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+lateinit var button : Button
+class MainActivity7 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main8)
+    }
+    fun onClick(view: View?) {
+        when(view?.id){
+            R.id.tv1->{
+                val webpage: Uri = Uri.parse("https://pidruchnyk.com.ua/432-storya-ukrayini-pometun-gupan-10-klas.html")
+                val intent = Intent(Intent.ACTION_VIEW, webpage)
+                if (intent.resolveActivity(packageManager) != null) {
+                    startActivity(intent)
+                }}
+        } }
+}
