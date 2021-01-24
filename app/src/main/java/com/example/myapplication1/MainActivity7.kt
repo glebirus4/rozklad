d76a38b7e1656d655d2a560427bbd90ae75a7fc5
glebirus4 <58050857+glebirus4@users.noreply.github.com>
2021-01-24 20:09:47 +0200

    one more

--- app/src/main/java/com/example/myapplication1/MainActivity7.kt
+++ app/src/main/java/com/example/myapplication1/MainActivity7.kt
@@ -13,23 +13,7 @@ class MainActivity7 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main8)
     }
-    fun onClick(view: View?) {
-        when(view?.id){
-            R.id.tv1->{
-                val webpage: Uri = Uri.parse("https://pidruchnyk.com.ua/432-storya-ukrayini-pometun-gupan-10-klas.html")
-                val intent = Intent(Intent.ACTION_VIEW, webpage)
-                if (intent.resolveActivity(packageManager) != null) {
-                    startActivity(intent)
-                }}
-        } }
 
-    fun onClick1(view: View?) {
-        when(view?.id){
-            R.id.tv2->{
-                val webpage: Uri = Uri.parse("https://pidruchnyk.com.ua/447-ekonomka-radonova-11-klas.html")
-                val intent = Intent(Intent.ACTION_VIEW, webpage)
-                if (intent.resolveActivity(packageManager) != null) {
-                    startActivity(intent)
-                }}
-        }}
+
+
 }
